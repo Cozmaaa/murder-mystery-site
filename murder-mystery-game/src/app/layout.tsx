0ef8432fbfdca8dc8afb46a7fdd5c2138e2ca106
unknown <luncancosmin@gmail.com>
2024-11-20 02:36:02 +0200

    Fixed the path of build.css import

--- murder-mystery-game/src/app/layout.tsx
+++ murder-mystery-game/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import '../builds.css'
+import '../../build.css';
 import { ReactNode } from "react";
 
 const geistSans = localFont({
